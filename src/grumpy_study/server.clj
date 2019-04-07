daecf4ac24bfb6a91bcf9c20abb4f219605219fc
Andrew Nee <ndrewnee@gmail.com>
2019-04-07 23:01:36 +0300

    Make simple web server

---
+++ src/grumpy_study/server.clj
@@ -0,0 +1,19 @@
+(ns grumpy-study.server
+  (:require
+    [immutant.web :as web]))
+
+(def app 
+  (fn [req]
+    { :status 200
+      :body (:uri req) }))
+
+(defn -main [& args]
+  (let [args-map (apply array-map args)
+        port-str (or (get args-map "-p")
+                     (get args-map "--port")
+                     "8080")]
+  (web/run #'app { :port (Integer/parseInt port-str) })))
+
+(comment
+  (def server (-main "--port" "8000"))
+  (web/stop server))
