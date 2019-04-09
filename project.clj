04e43b3ef996f19903f93e300c16330e757a57cb
Andrew Nee <ndrewnee@gmail.com>
2019-04-09 23:35:11 +0300

    Render dummy posts using rum

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [ring/ring-core "1.7.1"]
                  [org.immutant/web "2.1.10"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [rum "0.11.3"]]
   :repl-options {:init-ns grumpy-study.server})
--- src/grumpy_study/server.clj
+++ src/grumpy_study/server.clj
@@ -1,12 +1,37 @@
 (ns grumpy-study.server
   (:require
+   [rum.core :as rum]
    [immutant.web :as web]
    [compojure.core :as cj]
    [compojure.route :as cjr]))
 
+(def posts
+  [{:id "1"
+    :created #inst "2017-08-30"
+    :author "nikitonskiy"
+    :body "some body"}
+   {:id "2"
+    :created #inst "2017-08-29"
+    :author "freetonik"
+    :body "some body 2"}])
+
+(rum/defc post [post]
+  [:.post
+   [:.post_sidebar
+    [:img.avatar {:src (str "/i/" (:author post) ".jpg")}]]
+   [:div
+    [:p [:span.author (:author post) ": "] (:body post)]
+    [:p.meta (:created post) "//" [:a {:href (str "/post/" (:id post))} "Link"]]]])
+
+(rum/defc index [posts]
+  [:html
+   [:body
+    (for [p posts]
+      (post p))]])
+
 (cj/defroutes routes
   (cj/GET "/" [:as req]
-    {:body "INDEX"})
+    {:body (rum/render-static-markup (index posts))})
   (cj/GET "/write" [:as req]
     {:body "WRITE"})
   (cj/POST "/write" [:as req]
